ab3b8eb1823fef63b5ff0349dd213e3fe1ebbdfb
Kandiyah <ijegwajemimah@gmail.com>
2023-03-30 02:11:25 -0700

    width push

--- get_width.c
+++ get_width.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * get_width - Calculates the width for printing
+ * get_width - Calculates the width for printing.
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
